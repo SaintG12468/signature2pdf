d63504c7eeaf7beb131fccccdc7805ffbcfd1a99
fstgeorge <fabian@freecast.com>
2016-07-16 12:32:17 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    jshint = require('jshint'),
+    gjshint = require('gulp-jshint'),
+    browser = require('browser-sync'),
+    uglify = require('gulp-uglify');
+
+gulp.task('default', function() {
+    // place code for your default task here
+    console.log('Running Default');
+});
