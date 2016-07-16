2a7dc6935b1a3152e6afbff323d2488b9625804a
fstgeorge <fabian@freecast.com>
2016-07-16 14:10:07 -0400

    Add index.html to dist to moveto gh pages

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,19 @@ var gulp = require('gulp'),
     jshint = require('jshint'),
     gjshint = require('gulp-jshint'),
     browser = require('browser-sync'),
+    pages = require('gulp-gh-pages'),
     uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
     // place code for your default task here
     console.log('Running Default');
+});
+
+gulp.task('build', function () {
+    
+});
+
+gulp.task('deploy', function() {
+    return gulp.src('./dist/**/*')
+        .pipe(pages());
 });
